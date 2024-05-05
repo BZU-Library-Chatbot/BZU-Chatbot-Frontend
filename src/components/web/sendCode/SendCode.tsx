45de2fb7e1b393a023797e4cf68ccc2852325edf
Raghad-Aqel <raghad.saleem.aqel@gmail.com>
2024-05-05 12:59:29 +0300

    send code view

--- src/components/web/sendCode/SendCode.tsx
+++ src/components/web/sendCode/SendCode.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Input from "../../pages/Input";
+import { useFormik } from "formik";
+import { toast, Bounce } from "react-toastify";
+import { loginSchema } from "../validation/validate";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./SendCode.module.scss";
+import { jwtDecode } from "jwt-decode";
+import { useState, useEffect } from "react";
+import authService from "../../../services/authService";
+// import { sensCode } from "./api";
+import { useTranslation } from "react-i18next";
 
-export default function SendCode() {
+interface FormValues {
+    email: string;
+}
+
+const SendCode: React.FC = () => {
+  const { t } = useTranslation();
+  const [isAuth, setIsAuth] = useState(authService.isAuthenticated());
+  const navigate = useNavigate();
+
   return (
     <div>SendCode</div>
   )
 }
+
+export default SendCode;
