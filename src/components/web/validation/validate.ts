f78d64d53747ff9069c9c882c0476ac70ec737d7
Raghad-Aqel <raghad.saleem.aqel@gmail.com>
2024-05-09 15:14:51 +0300

    Update validate.ts

--- src/components/web/validation/validate.ts
+++ src/components/web/validation/validate.ts
@@ -68,4 +68,5 @@ export const changePasswordSchema = yup.object({
     .string()
     .oneOf([yup.ref("newPassword"), undefined], "Passwords must match")
     .required("Confirm Password is required"),
-});
+}); 
+
