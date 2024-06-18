edf37526835d66328b4153bd976ad69e1e4b6c82
Raghad-Aqel <raghad.saleem.aqel@gmail.com>
2024-06-18 14:20:30 +0300

    update routers depends on user roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
 import React, { useEffect } from "react";
-import Router from "./layouts/routes.tsx";
+import AdminRouter from "./layouts/AdminRouter.tsx";
+import UserRoutes from "./layouts/UserRouter.tsx";
 import languageService from "./services/languageService.ts";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
 import i18n from "./i18n/config.ts";
+import { setUser } from "./redux/userSlice.ts";
+import { useDispatch, useSelector } from "react-redux";
+import authService from "././services/authService.ts";
+import { getProfile } from "./common/api.ts";
+import { toast } from "react-toastify";
+import { useTranslation } from "react-i18next";
 
 const App: React.FC = () => {
+  const { user } = useSelector((state: any) => state.user);
+  const dispatch = useDispatch();
+  const { t }: any = useTranslation();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (!user && authService.isAuthenticated()) {
+        const response = await getProfile();
+        if (response?.status < 300) {
+          const userData = response.data.user;
+          dispatch(setUser(userData));
+        } else {
+          toast.error(`${t("global.serverError")}`, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      }
+    };
+    fetchUser();
+  }, []);
+
   useEffect(() => {
     const changeLanguage = async () => {
       if (!languageService.hasLanguage()) {
@@ -24,9 +59,17 @@ const App: React.FC = () => {
     }
   }, []);
 
+  const renderRoutes = () => {
+    if (user?.role === "Admin") {
+      return <AdminRouter />;
+    } else {
+      return <UserRoutes />;
+    } 
+  };
+
   return (
     <PrimeReactProvider>
-      <Router />
+      {renderRoutes()}
     </PrimeReactProvider>
   );
 };
